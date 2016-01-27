9d9af8ff402c1e6fe460e8739cda67e95866140d
Danial Goodwin <danialgoodwin@gmail.com>
2016-01-27 09:49:10 -0500

    Add RatingBar sample

--- api-sampler/app/src/main/java/net/simplyadvanced/androidapisampler/viewsample/RatingBarViewSample.java
+++ api-sampler/app/src/main/java/net/simplyadvanced/androidapisampler/viewsample/RatingBarViewSample.java
@@ -6,10 +6,12 @@ package net.simplyadvanced.androidapisampler.viewsample;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.AppCompatRatingBar;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.RatingBar;
+import android.widget.Toast;
 
 import net.simplyadvanced.androidapisampler.ViewSample;
 
@@ -20,6 +22,14 @@ public class RatingBarViewSample implements ViewSample {
 
     private Context mContext;
     private LinearLayout mRootView;
+    private LinearLayout mRootViewCompat;
+
+    private RatingBar.OnRatingBarChangeListener mOnRatingBarChangeListener = new RatingBar.OnRatingBarChangeListener() {
+        @Override
+        public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+            Toast.makeText(mContext, "rating=" + rating, Toast.LENGTH_SHORT).show();
+        }
+    };
 
     public RatingBarViewSample(@NonNull Context context) {
         mContext = context;
@@ -34,7 +44,7 @@ public class RatingBarViewSample implements ViewSample {
     @NonNull
     @Override
     public String getDescription() {
-        return "A simple programmatic usage, default implementation.";
+        return "A simple programmatic usage. 1) default implementation, 2) with setMax(5).";
     }
 
     @NonNull
@@ -42,10 +52,16 @@ public class RatingBarViewSample implements ViewSample {
     public View getView() {
         if (mRootView == null) {
             RatingBar rb1 = new RatingBar(mContext);
+            rb1.setOnRatingBarChangeListener(mOnRatingBarChangeListener);
+
+            RatingBar rb2 = new RatingBar(mContext);
+            rb2.setOnRatingBarChangeListener(mOnRatingBarChangeListener);
+            rb2.setMax(5);
 
             mRootView = new LinearLayout(mContext);
             mRootView.setOrientation(LinearLayout.VERTICAL);
             mRootView.addView(rb1);
+            mRootView.addView(rb2);
         }
         return mRootView;
     }
@@ -53,7 +69,14 @@ public class RatingBarViewSample implements ViewSample {
     @Nullable
     @Override
     public View getViewCompat() {
-        return null;
+        if (mRootViewCompat == null) {
+            AppCompatRatingBar rb1 = new AppCompatRatingBar(mContext);
+
+            mRootViewCompat = new LinearLayout(mContext);
+            mRootViewCompat.setOrientation(LinearLayout.VERTICAL);
+            mRootViewCompat.addView(rb1);
+        }
+        return mRootViewCompat;
     }
 
 }
